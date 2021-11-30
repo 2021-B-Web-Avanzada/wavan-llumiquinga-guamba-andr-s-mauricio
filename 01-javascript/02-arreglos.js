a6093bd512c6cf3bcae597eb7d893b99de35f28a
Andrés Llumiquinga <andres.llumiquinga@epn.edu.ec>
2021-11-30 00:16:37 -0500

    arreglos
     metodos

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -30,3 +30,27 @@ let objetoPrueba = {a: 1, b: 2, c:3};
 for(let llave in objetoPrueba){
     console.log("Objeto llave:", llave);
 }
+
+/* ---------- MÉTODOS  ----------  */
+
+console.log("Arreglo inicial:",arreglo);
+
+// PUSH --> Agregar uno o varios elementos al final
+arreglo.push(11);
+console.log("PUSH 11:",arreglo);
+//POP --> Eliminar el último elemento
+arreglo.pop();
+console.log("POP:",arreglo);
+//UNSHIFT --> Añadir al principio
+arreglo.unshift(5);
+console.log("UNSHIFT 5: ",arreglo);
+
+//SPLICE --> Eliminar/Añadir la posición y cantidad indicada
+arreglo.splice(3,0,4); //primer numero es el indice a agregar
+console.log("SPLICE ADD POSITION 0:",arreglo);
+//Encuetra el primer elemeto y devuelve el índice
+const indice = arreglo.indexOf(9);
+console.log(indice)
+arreglo.splice(indice,2); //desde el indice dado elimina la cantidad de numeros
+console.log("SPLICE DELETE 9 y 10:",arreglo);
+

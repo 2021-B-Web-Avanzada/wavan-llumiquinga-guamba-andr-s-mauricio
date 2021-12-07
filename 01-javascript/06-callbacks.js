c51851d3e31fd4a02feea341202d03b93b6f695a
Andrés Llumiquinga <andres.llumiquinga@epn.edu.ec>
2021-12-07 00:07:20 -0500

    class

--- 01-javascript/06-callbacks.js
+++ 01-javascript/06-callbacks.js
@@ -1,29 +1,26 @@
-//06-callbacks.js
-
-//Importar librería
-const fs = require("fs"); //file system
-console.log("Primero");
-
+// 06-callbacks.js
+const fs = require('fs'); // file system
+// 06-ejemplo.txt -> Hola
+console.log('PRIMERO');
 fs.readFile(
-    "./06-ejemplo.txt",
-    "utf-8",
-    (error,contenido) =>{
+    './06-ejemplo.txt', // 1
+    'utf-8',
+    (error, contenido) => {
         if(error){
             console.error({mensaje:'error leyendo contenido 06 ejemplo txt', error: error});
         }else{
             fs.readFile(
-                './01-variable.js', //2
-                "utf-8",
-                (errorVariable,contenidoVariable) =>{
+                './01-variables.js', // 2
+                'utf-8',
+                (errorVariable, contenidoVariable) => {
                     if(errorVariable){
-                        console.error({mensaje: "error leyendo contenido 01 varibale", error : errorVariable });
-                    } else{
+                        console.error({mensaje:'error leyendo contenido 01 variable', error: errorVariable});
+                    }else{
                         console.log(contenido, contenidoVariable);
                     }
                 }
-                );
+            );
         }
     }
 );
-
-console.log("Tercero");
+console.log('TERCERO');
